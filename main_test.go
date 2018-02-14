2d0946a21e85fbb1344a493c4f4950ce5eec5077
Max Prettyjohns <heshoots9999@gmail.com>
2018-02-14 23:01:05 +0000

    Add tests

---
+++ main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"testing"
+)
+
+func GetMockSession(userid string) *discordgo.Session {
+	var session *discordgo.Session
+	session = new(discordgo.Session)
+	session.State = new(discordgo.State)
+	session.State.User = new(discordgo.User)
+	session.State.User.ID = userid
+	return session
+}
+
+func GetMockMessageCreate(content string, authorID string) *discordgo.MessageCreate {
+	author := new(discordgo.User)
+	author.ID = authorID
+	var message *discordgo.Message
+	message = new(discordgo.Message)
+	message.Author = author
+	message.Content = content
+	var messageEvent *discordgo.MessageCreate
+	messageEvent = new(discordgo.MessageCreate)
+	messageEvent.Message = message
+	return messageEvent
+}
+
+func TestHasPrefix(t *testing.T) {
+	msg := GetMockMessageCreate("!hello world", "10")
+	res := hasPrefix("!hello", msg)
+	if res != true {
+		t.Error("Message contains prefix")
+	}
+	msg = GetMockMessageCreate("no !hello world", "10")
+	res = hasPrefix("!hello", msg)
+	if res != false {
+		t.Error("Prefix is not at beginning of message")
+	}
+	msg = GetMockMessageCreate("!test world", "10")
+	res = hasPrefix("!hello", msg)
+	if res != false {
+		t.Error("Prefix does not occur")
+	}
+	msg = GetMockMessageCreate("!hell", "10")
+	res = hasPrefix("!hello", msg)
+	if res != false {
+		t.Error("Message is shorter than prefix")
+	}
+}
+
+func TestPrefixHandler(t *testing.T) {
+	session := GetMockSession("10")
+	messageCreate := GetMockMessageCreate("!nocall", "10")
+	prefixed := prefixHandler("!nocall", func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		t.Error("Should not be called")
+	})
+	prefixed(session, messageCreate)
+	session = GetMockSession("11")
+	messageCreate = GetMockMessageCreate("!call", "10")
+	called := false
+	prefixed = prefixHandler("!call", func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		called = true
+	})
+	prefixed(session, messageCreate)
+	if called == false {
+		t.Error("Handler should have been called")
+	}
+}
